503a5e9139bb60782c1460c90b48c97dc4e63954
L65-ops <126471115+L65-ops@users.noreply.github.com>
2023-02-27 23:00:08 +0100

    Add files via upload

--- A1.py
+++ A1.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,77 +11,110 @@ import sklearn
 from sklearn import datasets, ensemble, metrics, svm, model_selection, linear_model
 
 
-def training_test_split(X, y, test_size=0.3, random_state=None):
-    """ Split the features X and labels y into training and test features and labels.
+# In[ ]:
 
+
+def training_test_split(X, y, test_size=0.3, random_state=None):
+    """ Split the features X and labels y into training and test features and labels. 
+    
     `split` indicates the fraction (rounded down) that should go to the test set.
 
     `random_state` allows to set a random seed to make the split reproducible. 
     If `random_state` is None, then no random seed will be set.
-
+    
     """
-    raise NotImplementedError('Your code here')
+     # Determine the number of instances in the dataset and determine the number of instances that should be allocated to the test set
+    n_instances = len(X)
+    n_test_instances = int(np.floor(test_size * n_instances))
+    
+    # Set the random seed if provided
+    if random_state is not None:
+        np.random.seed(random_state)
+    
+    # Randomly shuffle the indices of the instances
+    shuffled_indices = np.random.permutation(n_instances)
+    
+    # Select the instances that will be in the training and test sets
+    test_indices = shuffled_indices[:n_test_instances]
+    train_indices = shuffled_indices[n_test_instances:]
+    
+    # Split the data into training and test sets
+    X_train, X_test = X[train_indices], X[test_indices]
+    y_train, y_test = y[train_indices], y[test_indices]
+    
+    # raise NotImplementedError('Your code here')
     return X_train, X_test, y_train, y_test
 
 
-def true_positives(true_labels, predicted_labels, positive_class):
-    pos_true = true_labels == positive_class  # compare each true label with the positive class
-    pos_predicted = predicted_labels == positive_class # compare each predicted label to the positive class
-    match = pos_true & pos_predicted  # use logical AND (that's the `&`) to find elements that are True in both arrays
-    return np.sum(match)  # count them
-
-
-def false_positives(true_labels, predicted_labels, positive_class):
-    pos_predicted = predicted_labels == positive_class  # predicted to be positive class
-    neg_true = true_labels != positive_class  # actually negative class
-    match = pos_predicted & neg_true  # The `&` is element-wise logical AND
-    return np.sum(match)  # count the number of matches
+# In[ ]:
 
 
 def true_negatives(true_labels, predicted_labels, positive_class):
-    raise NotImplementedError('Your code here')
+    neg_true = true_labels != positive_class  # actually negative class
+    neg_predicted = predicted_labels != positive_class # predicted to be negative class
+    match = neg_true & neg_predicted # use logical AND (that's the `&`) to find elements that are True in both arrays
+    #raise NotImplementedError('Your code here')
+    return np.sum(match)  # count them
 
 
 def false_negatives(true_labels, predicted_labels, positive_class):
-    raise NotImplementedError('Your code here')
+    neg_predicted = predicted_labels != positive_class  # predicted to be negative class
+    pos_true = true_labels == positive_class  # actually positive class
+    match = neg_predicted & pos_true  # The `&` is element-wise logical AND
+    #raise NotImplementedError('Your code here')
+    return np.sum(match)  # count the number of matches
 
 
-def precision(true_labels, predicted_labels, positive_class):
-    TP = true_positives(true_labels, predicted_labels, positive_class)
-    FP = false_positives(true_labels, predicted_labels, positive_class)
-    return TP / (TP + FP)
+# In[ ]:
 
 
 def recall(true_labels, predicted_labels, positive_class):
-    raise NotImplementedError('Your code here')
+    TP = true_positives(true_labels, predicted_labels, positive_class)
+    FN = false_negatives(true_labels, predicted_labels, positive_class)
+    return TP / (TP + FN)
+
+
+# In[ ]:
 
 
 def accuracy(true_labels, predicted_labels, positive_class):
-    raise NotImplementedError('Your code here')
+    FP = false_positives(true_labels, predicted_labels, positive_class)
+    TN = true_negatives(true_labels, predicted_labels, positive_class)
+    TP = true_positives(true_labels, predicted_labels, positive_class)
+    FN = false_negatives(true_labels, predicted_labels, positive_class)
+    return (TP + TN)/(FP+TN+TP+FN)
+
+
+# In[ ]:
 
 
 def specificity(true_labels, predicted_labels, positive_class):
-    raise NotImplementedError('Your code here')
+    FP = false_positives(true_labels, predicted_labels, positive_class)
+    TN = true_negatives(true_labels, predicted_labels, positive_class)
+    return TN/(FP+TN)
+
+
+# In[ ]:
 
 
 def balanced_accuracy(true_labels, predicted_labels, positive_class):
-    raise NotImplementedError('Your code here')
+    FP = false_positives(true_labels, predicted_labels, positive_class)
+    TN = true_negatives(true_labels, predicted_labels, positive_class)
+    return TN/(FP+TN)
+
+
+# In[ ]:
 
 
 def F1(true_labels, predicted_labels, positive_class):
-    raise NotImplementedError('Your code here')
+    #had to add a 1 because 'precision/recall' was already mentioned in previous statements.
+    precision1 = precision(true_labels, predicted_labels, positive_class)
+    recall1 = recall(true_labels, predicted_labels, positive_class)
+    return 2*(precision1*recall1)/(precision1+recall1)
 
 
-def load_data(fraction=0.75, seed=None, target_digit=9, appply_stratification=True):
-    data = sklearn.datasets.load_digits()
-    X = data.data
-    y = data.target
-    y[y != target_digit] = 11  # we have to do this swap because 1 and 0 also occur as labels in our dataset
-    y[y == target_digit] = 12
-    y[y == 11] = 0  # negative class
-    y[y == 12] = 1  # positive class
-    if appply_stratification:
-        stratify = y
-    else:
-        stratify = None
-    return sklearn.model_selection.train_test_split(X, y, train_size=fraction, random_state=seed, stratify=stratify)
+# In[ ]:
+
+
+#Could not find exercise 8 anymore?? But I will upload the not skeleton version as well as A1Leo.
+
